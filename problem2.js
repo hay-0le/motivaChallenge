1bd5b85583d91041e702adb351ab3ed86d187312
hay-0le <haley.olsen1@gmail.com>
2020-01-28 09:41:06 -0800

    Complete problem 2 --all tests passing

--- problem2.js
+++ problem2.js
@@ -9,52 +9,67 @@
 //kebab case: this-is-kebab-case
 
        
+
 const { camelCase, kebabCase} = require('case-anything');
 
 let traverser = (converterFunction, obj, firstRun = true) => {
-
-   //Check that inputs are a function and an object (not an array) on the first run ---> leaving firstRun argument off defaults to true which will run check
     
+   //Check that inputs are a function and an object (not an array) on the first run ---> leaving firstRun argument off defaults to true which will run check
+    if (firstRun && !(typeof converterFunction === 'function' && !Array.isArray(obj) && typeof obj === 'object')) {
+        return 'Error: incorrect input types. Please invoke with a function and an object.';
+    }
 
     //helper function to change case type of key with converterFunction, then updates the obj with newKey
-
+    let updateKey = (key) => {
+        let newKey = converterFunction(key);
         //set newKey in obj equal to values at old key
-       
+        obj[newKey] = obj[key];
         //delete old key
-       
-        //return new key
+        delete obj[key];
+        
+        return newKey;
+    }
+
 
     //-- Checks to handle different data types --
     //if obj is an array
-    
+    if (Array.isArray(obj)) {
         //create empty results array to add all converted values to
-        
+        let results = [];
         //loop through elements in the array
-        
+        for (let element of obj) {
             //add the element (converted) to the array, using traverser recursively to handle nested data
-        
+            results.push(traverser(converterFunction, element, false))
+        }
         //update obj to converted array
-        
-    //if obj is an object
-   
-        //create cache for checked keys to keep from infinite loop since keys are being updated
-       
-        //loop through each key
-      
-            //use cache to see if key has been converted already
-         
-                //use helper function to convert case type of the key (also updates it in the obj)
-                
-                //get converted value, using traverser recursively to handle nested data (set firstRun to false to avoid incorrect checks of values)
-              
-                //update object with converted key/value pair
-              
-                //add newKey to checkedKeys cache
-        
-    //obj isn't array, object, or function so it will be primitive data type
-  
-        //convert it to different case
+        obj = results;
 
+
+    //if obj is an object
+    } else if (typeof obj === 'object') {
+        //create cache for checked keys to keep from infinite loop since keys are being updated
+        let checkedKeys = {};
+        //loop through each key
+        for (let key in obj) {
+            //use cache to see if key has been converted already
+            if (!checkedKeys[key]) {
+                //use helper function to convert case type of the key (also updates it in the obj)
+                let newKey = updateKey(key);
+                //get converted value, using traverser recursively to handle nested data (set firstRun to false to avoid incorrect checks of values)
+                let newValue = traverser(converterFunction, obj[newKey], false);
+                //update object with converted key/value pair
+                obj[newKey] = newValue;
+                //add newKey to checkedKeys cache
+                checkedKeys[newKey] = true;
+            }
+        }
+        
+
+    //obj isn't array, object, or function so it will be primitive data type
+    } else {
+        //convert it to different case
+        obj = converterFunction(obj);
+    }
 
     //return converted obj;
     return obj;
