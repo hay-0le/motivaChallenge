3ed4eddf8a8b75bf3d420b4beb317bfb96d0eb53
hay-0le <haley.olsen1@gmail.com>
2020-01-28 18:53:50 -0800

    Renamed imported function to make function names more clear

--- tests/problem1.test.js
+++ tests/problem1.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { createIntervals } = require('../problem1.js');
+const { createDateIntervals } = require('../problem1.js');
 
 //check for deep equality of nested arrays
 let arraysMatch = (arr1, arr2) => {
@@ -18,13 +18,13 @@ let arraysMatch = (arr1, arr2) => {
 
 describe('Tests for correct data types:', () => {
 
-    it('CreateIntervals returns an array', () => {
-        let result = createIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 7);
+    it('createDateIntervals returns an array', () => {
+        let result = createDateIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 7);
         expect(Array.isArray(result)).to.equal(true);
     })
 
-    it('CreateIntervals is a function', () => {
-        expect(typeof createIntervals === 'function').to.equal(true);
+    it('createDateIntervals is a function', () => {
+        expect(typeof createDateIntervals === 'function').to.equal(true);
     })
 })
 
@@ -32,25 +32,25 @@ describe('Tests for correct data types:', () => {
 describe('Tests for correct input:', () => {
 
     it('Start date must be earlier than end date', () => {
-        let result = createIntervals('2022-12-14 06:14:56', '2020-02-14 06:14:56', 7);
+        let result = createDateIntervals('2022-12-14 06:14:56', '2020-02-14 06:14:56', 7);
 
         expect(result).to.equal('Invalid input dates. Start dates must occur before end date');
     })
 
     it('Should return one tuple in an array, if periodOfDays is more than different between start and end', () => {
-        let result = createIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 90)
+        let result = createDateIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 90)
         
         expect(arraysMatch(result, [ [ '2019-12-14', '2020-2-14' ] ])).to.equal(true);
     })
 
     it('Both dates are correctly formatted', () => {
-        let result = createIntervals('19-12-14 06:14:56', '2020-02-14 06:14:56', 90)
+        let result = createDateIntervals('19-12-14 06:14:56', '2020-02-14 06:14:56', 90)
 
         expect(result).to.equal('Start and end are not in correct "YYYY-MM-DD hh:mm:ss" format');
     })
 
     it('Returns error when inputs are incorrect data types', () => {
-        let result = createIntervals('19-12-14 06:14:56', '2020-02-14 06:14:56', '90')
+        let result = createDateIntervals('19-12-14 06:14:56', '2020-02-14 06:14:56', '90')
 
         expect(result).to.equal('Invalid input types: The first two arguments should be strings, while the second should be a number.');
     })
@@ -60,7 +60,7 @@ describe('Tests for correct input:', () => {
 describe('Tests for correct outputs:', () => {
 
     it('Returns correct result when periodOfDays is less than gap between start and end dates', () => {
-        let result = createIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 7);
+        let result = createDateIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 7);
         let expectedResult = [
             [ '2019-12-14', '2019-12-21' ],
             [ '2019-12-21', '2019-12-28' ],
@@ -77,7 +77,7 @@ describe('Tests for correct outputs:', () => {
     })
 
     it('Returns correct result when periodOfDays is more than gap between start and end dates', () => {
-        let result = createIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 90)
+        let result = createDateIntervals('2019-12-14 06:14:56', '2020-02-14 06:14:56', 90)
         let expectedResult = [ ['2019-12-14', '2020-2-14'] ]
 
         expect(arraysMatch(result, expectedResult)).to.equal(true);
